e84a892b4ffd0a6566ae9f6362295c5d887af5ab
Jan Pokorný <JenomPokorny@gmail.com>
2025-07-03 15:48:55 +0200

    fix: beeai-web build
    Signed-off-by: Jan Pokorný <JenomPokorny@gmail.com>

--- apps/beeai-web/src/constants.ts
+++ apps/beeai-web/src/constants.ts
@@ -5,10 +5,8 @@
 
 import packageJson from '../package.json';
 
-export const RELEASE_VERSION = `v${packageJson.version}`;
-
 export const DOCKER_MANIFEST_LABEL_NAME = 'beeai.dev.agent.yaml';
 
-export const AGENT_REGISTRY_URL = `https://raw.githubusercontent.com/i-am-bee/beeai/refs/heads/v${RELEASE_VERSION}/agent-registry.yaml`;
+export const AGENT_REGISTRY_URL = `https://raw.githubusercontent.com/i-am-bee/beeai/refs/tags/v${packageJson.version}/agent-registry.yaml`;
 
 export const SupportedDockerRegistries = ['ghcr.io'];
