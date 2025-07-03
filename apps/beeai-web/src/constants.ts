535beb2feea284def40b645ed5823875dc444ef0
Jan Pokorný <JenomPokorny@gmail.com>
2025-07-03 15:40:31 +0200

    feat: make agent-registry fixed to current version
    Signed-off-by: Jan Pokorný <JenomPokorny@gmail.com>

--- apps/beeai-web/src/constants.ts
+++ apps/beeai-web/src/constants.ts
@@ -3,10 +3,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export const RELEASE_VERSION = 'v0.1.2';
+import packageJson from '../package.json';
+
+export const RELEASE_VERSION = `v${packageJson.version}`;
 
 export const DOCKER_MANIFEST_LABEL_NAME = 'beeai.dev.agent.yaml';
 
-export const AGENT_REGISTRY_URL = `https://raw.githubusercontent.com/i-am-bee/beeai/refs/heads/release-${RELEASE_VERSION}/agent-registry.yaml`;
+export const AGENT_REGISTRY_URL = `https://raw.githubusercontent.com/i-am-bee/beeai/refs/heads/v${RELEASE_VERSION}/agent-registry.yaml`;
 
 export const SupportedDockerRegistries = ['ghcr.io'];
