06b8789c25d320fff0d32dbd8988cff7cfd4e670
Jan Pokorný <JenomPokorny@gmail.com>
2025-07-22 15:12:40 +0200

    feat(beeai-sdk): implement LLM service extension
    Signed-off-by: Jan Pokorný <JenomPokorny@gmail.com>

--- apps/beeai-sdk/src/beeai_sdk/a2a_extensions/__init__.py
+++ apps/beeai-sdk/src/beeai_sdk/a2a_extensions/__init__.py
@@ -1,3 +1,22 @@
 # Copyright 2025 © BeeAI a Series of LF Projects, LLC
 # SPDX-License-Identifier: Apache-2.0
 
+from __future__ import annotations
+
+import typing
+
+import a2a.types
+
+ParamsT = typing.TypeVar("ParamsT")
+MetadataT = typing.TypeVar("MetadataT")
+
+
+class Extension(typing.Protocol, typing.Generic[ParamsT, MetadataT]):
+    URI: str
+    Params: type[ParamsT]
+    Metadata: type[MetadataT]
+
+    @classmethod
+    def from_agent_card(cls, agent: a2a.types.AgentCard) -> typing.Self | None: ...
+    def to_agent_card_extension(self, *, required: bool) -> a2a.types.AgentExtension: ...
+    def read_metadata(self, message: a2a.types.Message) -> MetadataT | None: ...
--- apps/beeai-sdk/src/beeai_sdk/a2a_extensions/services/llm.py
+++ apps/beeai-sdk/src/beeai_sdk/a2a_extensions/services/llm.py
@@ -1,12 +1,17 @@
 # Copyright 2025 © BeeAI a Series of LF Projects, LLC
 # SPDX-License-Identifier: Apache-2.0
 
+from __future__ import annotations
+
 import typing
 
+import a2a.types
 import pydantic
 
+import beeai_sdk.a2a_extensions
 
-class LlmModelFeatures(pydantic.BaseModel):
+
+class LLMFeatures(pydantic.BaseModel):
     streaming: bool = False
     """Model supports streaming responses (`stream`, `stream_options`)"""
 
@@ -38,11 +43,7 @@ class LlmModelFeatures(pydantic.BaseModel):
     """Indicates areas, languages, tasks etc. the model is specialized in."""
 
 
-class LlmExtensionParams(pydantic.BaseModel):
-    model_requests: dict[str, LlmModelFeatures]
-
-
-class ProvidedModel(pydantic.BaseModel):
+class LLMAnswer(pydantic.BaseModel):
     identifier: str | None = None
     """
     Name of the model for identification and optimization purposes.
@@ -50,7 +51,7 @@ class ProvidedModel(pydantic.BaseModel):
     (This does not necessarily mean that the model is provided by Ollama or OpenRouter, it is just used for model identification.)
     """
 
-    features: LlmModelFeatures | None = None
+    features: LLMFeatures | None = None
     """Features that the model supports. This is useful when the agent can optionally use a feature but has a fallback."""
 
     api_base: str
@@ -58,21 +59,56 @@ class ProvidedModel(pydantic.BaseModel):
     api_model: str
 
 
-class ClientMessageMetadata(pydantic.BaseModel):
-    provided_models: dict[str, ProvidedModel] = {}
-    """Provided models corresponding to the model requests."""
-
-
-class LlmModelRequest(pydantic.BaseModel):
+class LLMAsk(pydantic.BaseModel):
     description: str | None = None
     """Free-form description of how the model will be used."""
 
-    features: LlmModelFeatures | None = None
+    features: LLMFeatures | None = None
     """Requested minimal features of the model."""
 
     suggested: tuple[str, ...] = ()
     """
-    Model ids that should work best with this agent.
+    Model identifiers that should work best with this agent.
     Should be the Ollama model name if available (ex. "granite3.3:8b"), or OpenRouter model id under the primary provider (ex. "openai/gpt-4o").
     (This does not necessarily mean that the model should be provided by Ollama or OpenRouter, it is just used for model identification.)
     """
+
+
+class LLMServiceExtension(beeai_sdk.a2a_extensions.Extension):
+    URI = "https://a2a-extensions.beeai.dev/services/llm"
+
+    class Params(pydantic.BaseModel):
+        llm_asks: dict[str, LLMAsk]
+        """Model requests that the agent requires to be provided by the client."""
+
+    class Metadata(pydantic.BaseModel):
+        llm_anwers: dict[str, LLMAnswer] = {}
+        """Provided models corresponding to the model requests."""
+
+    def __init__(self, llm_asks: dict[str, LLMAsk]) -> None:
+        self.llm_asks = llm_asks
+
+    @classmethod
+    def from_agent_card(cls, agent: a2a.types.AgentCard) -> LLMServiceExtension | None:
+        try:
+            return LLMServiceExtension(
+                llm_asks=LLMServiceExtension.Params.model_validate(
+                    next(x for x in agent.capabilities.extensions or [] if x.uri == cls.URI).params
+                ).llm_asks
+            )
+        except StopIteration:
+            return None
+
+    def to_agent_card_extension(self, *, required: bool) -> a2a.types.AgentExtension:
+        return a2a.types.AgentExtension(
+            uri=self.URI,
+            description="Agent requests the client to provide LLMs for the agent to use.",
+            params=LLMServiceExtension.Params(llm_asks=self.llm_asks).model_dump(mode="json"),
+            required=required,
+        )
+
+    def read_metadata(self, message: a2a.types.Message) -> LLMServiceExtension.Metadata | None:
+        raw_metadata = getattr(message, "metadata", {}).get(self.URI, None)
+        if raw_metadata is None:
+            return None
+        return LLMServiceExtension.Metadata.model_validate(raw_metadata)
