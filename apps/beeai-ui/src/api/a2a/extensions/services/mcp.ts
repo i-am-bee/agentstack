i-am-bee/beeai-platform

feat: MCP demands handling in GUI
Signed-off-by: Tomas Weiss <tomas.weiss2@gmail.com>

apps/beeai-ui/src/api/a2a/extensions/service/mcp.ts
@@ -1,0 +1,57 @@
+/**
+ * Copyright 2025 © BeeAI a Series of LF Projects, LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { z } from 'zod';
+
+import type { A2AServiceExtension } from '../types';
+
+const URI = 'https://a2a-extensions.beeai.dev/services/mcp/v1';
+
+const mcpTransportTypesEnum = z.enum(['streamable_http', 'stdio']);
+type MCPTransportType = z.infer<typeof mcpTransportTypesEnum>;
+
+const mcpDemandSchema = z.object({
+  description: z.string().nullable(),
+  suggested: z.array(z.string()).nullable(),
+  allowed_transports: z.array(mcpTransportTypesEnum).nullable(),
+});
+
+export const demandsSchema = z.object({
+  mcp_demands: z.record(z.string(), mcpDemandSchema),
+});
+export type MCPDemand = z.infer<typeof demandsSchema>;
+
+const fullfilmentSchema = z.object({
+  mcp_fulfillments: z.record(
+    z.string(),
+    z.object({
+      transport: z.object({
+        type: mcpTransportTypesEnum,
+        url: z.string(),
+      }),
+    }),
+  ),
+});
+export type MCPFullfilment = z.infer<typeof fullfilmentSchema>;
+
+export const mcpExtension: A2AServiceExtension<
+  typeof URI,
+  z.infer<typeof demandsSchema>,
+  {
+    mcp_fulfillments: Record<
+      string,
+      {
+        transport: {
+          type: MCPTransportType;
+          url: string;
+        };
+      }
+    >;
+  }
+> = {
+  getUri: () => URI,
+  getDemandsSchema: () => demandsSchema,
+  getFullfilmentSchema: () => fullfilmentSchema,
+};

apps/beeai-ui/src/api/a2a/extensions/types.ts
@@ -5,7 +5,16 @@
 
 import type { z } from 'zod';
 
-export interface A2AExtension<U extends string, D> {
-  getSchema: () => z.ZodSchema<Partial<Record<U, D>>>;
+export interface A2AExtension<U extends string> {
   getUri: () => U;
+}
+
+export interface A2AComponentExtension<U extends string, D> extends A2AExtension<U> {
+  getMessageSchema: () => z.ZodSchema<Partial<Record<U, D>>>;
+}
+
+export interface A2AServiceExtension<U extends string, D, F> extends A2AExtension<U> {
+  getUri: () => U;
+  getDemandsSchema: () => z.ZodSchema<D>;
+  getFullfilmentSchema: () => z.ZodSchema<F>;
 }

apps/beeai-ui/src/api/a2a/extensions/component/citation.ts
@@ -5,7 +5,7 @@
 
 import { z } from 'zod';
 
-import type { A2AExtension } from './types';
+import type { A2AComponentExtension } from '../types';
 
 const URI = 'https://a2a-extensions.beeai.dev/ui/citation/v1';
 
@@ -27,7 +27,7 @@ export type CitationMetadata = z.infer<typeof schema>;
 
 export type Citation = z.infer<typeof citationSchema>;
 
-export const citationExtension: A2AExtension<typeof URI, CitationMetadata> = {
-  getSchema: () => z.object({ [URI]: schema }).partial(),
+export const citationExtension: A2AComponentExtension<typeof URI, CitationMetadata> = {
+  getMessageSchema: () => z.object({ [URI]: schema }).partial(),
   getUri: () => URI,
 };

apps/beeai-ui/src/api/a2a/extensions/component/trajectory.ts
@@ -5,7 +5,7 @@
 
 import { z } from 'zod';
 
-import type { A2AExtension } from './types';
+import type { A2AComponentExtension } from '../types';
 
 const URI = 'https://a2a-extensions.beeai.dev/ui/trajectory/v1';
 
@@ -18,7 +18,7 @@ const schema = z
 
 export type TrajectoryMetadata = z.infer<typeof schema>;
 
-export const trajectoryExtension: A2AExtension<typeof URI, TrajectoryMetadata> = {
-  getSchema: () => z.object({ [URI]: schema }).partial(),
+export const trajectoryExtension: A2AComponentExtension<typeof URI, TrajectoryMetadata> = {
+  getMessageSchema: () => z.object({ [URI]: schema }).partial(),
   getUri: () => URI,
 };

apps/beeai-ui/src/api/a2a/extensions/utils.ts
@@ -3,10 +3,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import type { A2AExtension } from './types';
+import type { AgentExtension } from '@a2a-js/sdk';
 
-export function getExtensionData<U extends string, D>(extension: A2AExtension<U, D>) {
-  const schema = extension.getSchema();
+import type { A2AComponentExtension, A2AServiceExtension } from './types';
+
+export function extractComponentExtensionData<U extends string, D>(extension: A2AComponentExtension<U, D>) {
+  const schema = extension.getMessageSchema();
   const uri = extension.getUri();
 
   return function (metadata: Record<string, unknown> | undefined) {
@@ -17,5 +19,28 @@ export function getExtensionData<U extends string, D>(extension: A2AExtension<U,
     }
 
     return parsed[uri];
+  };
+}
+
+export function extractServiceExtensionDemands<U extends string, D, F>(extension: A2AServiceExtension<U, D, F>) {
+  return function (agentExtensions: AgentExtension[]) {
+    const foundExtension = agentExtensions.find(({ uri }) => uri === extension.getUri());
+    if (!foundExtension) {
+      return null;
+    }
+
+    const data = extension.getDemandsSchema().parse(foundExtension.params);
+    return data;
+  };
+}
+
+export function fullfilServiceExtensionDemand<U extends string, D, F>(extension: A2AServiceExtension<U, D, F>) {
+  return (metadata: Record<string, unknown>, fullfillment: F) => {
+    const fullfilment = extension.getFullfilmentSchema().parse(fullfillment);
+
+    return {
+      ...metadata,
+      [extension.getUri()]: fullfilment,
+    };
   };
 }

apps/beeai-ui/src/api/a2a/utils.ts
@@ -18,15 +18,15 @@ import { UIMessagePartKind } from '#modules/messages/types.ts';
 import type { ContextId, TaskId } from '#modules/tasks/api/types.ts';
 import { isNotNull } from '#utils/helpers.ts';
 
-import type { Citation } from './extensions/citation';
-import { citationExtension } from './extensions/citation';
-import type { TrajectoryMetadata } from './extensions/trajectory';
-import { trajectoryExtension } from './extensions/trajectory';
-import { getExtensionData } from './extensions/utils';
+import type { Citation } from './extensions/component/citation';
+import { citationExtension } from './extensions/component/citation';
+import type { TrajectoryMetadata } from './extensions/component/trajectory';
+import { trajectoryExtension } from './extensions/component/trajectory';
+import { extractComponentExtensionData } from './extensions/utils';
 
-export const extractCitation = getExtensionData(citationExtension);
+export const extractCitation = extractComponentExtensionData(citationExtension);
 
-export const extractTrajectory = getExtensionData(trajectoryExtension);
+export const extractTrajectory = extractComponentExtensionData(trajectoryExtension);
 
 export function extractTextFromMessage(message: Message | undefined) {
   const text = message?.parts
@@ -70,10 +70,12 @@ export function createUserMessage({
   message,
   contextId,
   taskId,
+  metadata,
 }: {
   message: UIUserMessage;
   contextId: ContextId;
   taskId?: TaskId;
+  metadata?: Record<string, unknown>;
 }): Message {
   return {
     kind: 'message',
@@ -82,6 +84,7 @@ export function createUserMessage({
     contextId,
     taskId,
     parts: convertMessageParts(message.parts),
+    metadata,
   };
 }
 

i-am-bee/agentstack

apps/beeai-ui/src/api/a2a/extensions/services/mcp.ts
@@ -1,0 +1,57 @@
+/**
+ * Copyright 2025 © BeeAI a Series of LF Projects, LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { z } from 'zod';
+
+import type { A2AServiceExtension } from '../types';
+
+const URI = 'https://a2a-extensions.beeai.dev/services/mcp/v1';
+
+const mcpTransportTypesEnum = z.enum(['streamable_http', 'stdio']);
+type MCPTransportType = z.infer<typeof mcpTransportTypesEnum>;
+
+const mcpDemandSchema = z.object({
+  description: z.string().nullable(),
+  suggested: z.array(z.string()).nullable(),
+  allowed_transports: z.array(mcpTransportTypesEnum).nullable(),
+});
+
+export const demandsSchema = z.object({
+  mcp_demands: z.record(z.string(), mcpDemandSchema),
+});
+export type MCPDemand = z.infer<typeof demandsSchema>;
+
+const fulfillmentSchema = z.object({
+  mcp_fulfillments: z.record(
+    z.string(),
+    z.object({
+      transport: z.object({
+        type: mcpTransportTypesEnum,
+        url: z.string(),
+      }),
+    }),
+  ),
+});
+export type MCPFulfillment = z.infer<typeof fulfillmentSchema>;
+
+export const mcpExtension: A2AServiceExtension<
+  typeof URI,
+  z.infer<typeof demandsSchema>,
+  {
+    mcp_fulfillments: Record<
+      string,
+      {
+        transport: {
+          type: MCPTransportType;
+          url: string;
+        };
+      }
+    >;
+  }
+> = {
+  getUri: () => URI,
+  getDemandsSchema: () => demandsSchema,
+  getFulfillmentSchema: () => fulfillmentSchema,
+};

apps/beeai-ui/src/api/a2a/extensions/types.ts
@@ -5,7 +5,16 @@
 
 import type { z } from 'zod';
 
-export interface A2AExtension<U extends string, D> {
-  getSchema: () => z.ZodSchema<Partial<Record<U, D>>>;
+export interface A2AExtension<U extends string> {
   getUri: () => U;
+}
+
+export interface A2AUiExtension<U extends string, D> extends A2AExtension<U> {
+  getMessageMetadataSchema: () => z.ZodSchema<Partial<Record<U, D>>>;
+}
+
+export interface A2AServiceExtension<U extends string, D, F> extends A2AExtension<U> {
+  getUri: () => U;
+  getDemandsSchema: () => z.ZodSchema<D>;
+  getFulfillmentSchema: () => z.ZodSchema<F>;
 }

apps/beeai-ui/src/api/a2a/extensions/ui/citation.ts
@@ -5,7 +5,7 @@
 
 import { z } from 'zod';
 
-import type { A2AExtension } from './types';
+import type { A2AUiExtension } from '../types';
 
 const URI = 'https://a2a-extensions.beeai.dev/ui/citation/v1';
 
@@ -27,7 +27,7 @@ export type CitationMetadata = z.infer<typeof schema>;
 
 export type Citation = z.infer<typeof citationSchema>;
 
-export const citationExtension: A2AExtension<typeof URI, CitationMetadata> = {
-  getSchema: () => z.object({ [URI]: schema }).partial(),
+export const citationExtension: A2AUiExtension<typeof URI, CitationMetadata> = {
+  getMessageMetadataSchema: () => z.object({ [URI]: schema }).partial(),
   getUri: () => URI,
 };

apps/beeai-ui/src/api/a2a/extensions/ui/trajectory.ts
@@ -5,7 +5,7 @@
 
 import { z } from 'zod';
 
-import type { A2AExtension } from './types';
+import type { A2AUiExtension } from '../types';
 
 const URI = 'https://a2a-extensions.beeai.dev/ui/trajectory/v1';
 
@@ -18,7 +18,7 @@ const schema = z
 
 export type TrajectoryMetadata = z.infer<typeof schema>;
 
-export const trajectoryExtension: A2AExtension<typeof URI, TrajectoryMetadata> = {
-  getSchema: () => z.object({ [URI]: schema }).partial(),
+export const trajectoryExtension: A2AUiExtension<typeof URI, TrajectoryMetadata> = {
+  getMessageMetadataSchema: () => z.object({ [URI]: schema }).partial(),
   getUri: () => URI,
 };

apps/beeai-ui/src/api/a2a/extensions/utils.ts
@@ -3,19 +3,51 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import type { A2AExtension } from './types';
+import type { AgentExtension } from '@a2a-js/sdk';
 
-export function getExtensionData<U extends string, D>(extension: A2AExtension<U, D>) {
-  const schema = extension.getSchema();
+import type { A2AServiceExtension, A2AUiExtension } from './types';
+
+export function extractUiExtensionData<U extends string, D>(extension: A2AUiExtension<U, D>) {
+  const schema = extension.getMessageMetadataSchema();
   const uri = extension.getUri();
 
   return function (metadata: Record<string, unknown> | undefined) {
     const { success, data: parsed } = schema.safeParse(metadata ?? {});
 
     if (!success) {
-      return undefined;
+      return null;
     }
 
     return parsed[uri];
+  };
+}
+
+export function extractServiceExtensionDemands<U extends string, D, F>(extension: A2AServiceExtension<U, D, F>) {
+  const schema = extension.getDemandsSchema();
+  const uri = extension.getUri();
+
+  return function (agentExtensions: AgentExtension[]) {
+    const foundExtension = agentExtensions.find((agentExtension) => agentExtension.uri === uri);
+    const { success, data: parsed } = schema.safeParse(foundExtension?.params ?? {});
+
+    if (!success) {
+      return null;
+    }
+
+    return parsed;
+  };
+}
+
+export function fulfillServiceExtensionDemand<U extends string, D, F>(extension: A2AServiceExtension<U, D, F>) {
+  const schema = extension.getFulfillmentSchema();
+  const uri = extension.getUri();
+
+  return function (metadata: Record<string, unknown>, fulfillment: F) {
+    const { success, data: parsed } = schema.safeParse(fulfillment);
+
+    return {
+      ...metadata,
+      [uri]: success ? parsed : {},
+    };
   };
 }

apps/beeai-ui/src/api/a2a/utils.ts
@@ -18,15 +18,15 @@ import { UIMessagePartKind } from '#modules/messages/types.ts';
 import type { ContextId, TaskId } from '#modules/tasks/api/types.ts';
 import { isNotNull } from '#utils/helpers.ts';
 
-import type { Citation } from './extensions/citation';
-import { citationExtension } from './extensions/citation';
-import type { TrajectoryMetadata } from './extensions/trajectory';
-import { trajectoryExtension } from './extensions/trajectory';
-import { getExtensionData } from './extensions/utils';
+import type { Citation } from './extensions/ui/citation';
+import { citationExtension } from './extensions/ui/citation';
+import type { TrajectoryMetadata } from './extensions/ui/trajectory';
+import { trajectoryExtension } from './extensions/ui/trajectory';
+import { extractUiExtensionData } from './extensions/utils';
 
-export const extractCitation = getExtensionData(citationExtension);
+export const extractCitation = extractUiExtensionData(citationExtension);
 
-export const extractTrajectory = getExtensionData(trajectoryExtension);
+export const extractTrajectory = extractUiExtensionData(trajectoryExtension);
 
 export function extractTextFromMessage(message: Message | undefined) {
   const text = message?.parts
@@ -72,10 +72,12 @@ export function createUserMessage({
   message,
   contextId,
   taskId,
+  metadata,
 }: {
   message: UIUserMessage;
   contextId: ContextId;
   taskId?: TaskId;
+  metadata?: Record<string, unknown>;
 }): Message {
   return {
     kind: 'message',
@@ -84,6 +86,7 @@ export function createUserMessage({
     contextId,
     taskId,
     parts: convertMessageParts(message.parts),
+    metadata,
   };
 }
 