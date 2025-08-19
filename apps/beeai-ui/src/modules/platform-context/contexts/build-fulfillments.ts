49a00f4cea9fe5f330ff99aab5bbc1892bdf2ca0
Tomas Weiss <tomas.weiss2@gmail.com>
2025-08-19 10:27:07 +0200

    feat: serving multiple LLM demands properly
    Signed-off-by: Tomas Weiss <tomas.weiss2@gmail.com>

--- apps/beeai-ui/src/modules/platform-context/contexts/build-fulfillments.ts
+++ apps/beeai-ui/src/modules/platform-context/contexts/build-fulfillments.ts
@@ -10,17 +10,25 @@ export const buildFullfilments = (platformToken: string): Fulfillments => {
     mcp: async () => {
       throw new Error('MCP fulfillment not implemented');
     },
-    llm: async () => {
-      return {
-        llm_fulfillments: {
-          default: {
+    llm: async ({ llm_demands }) => {
+      const allDemands = Object.keys(llm_demands);
+      if (allDemands.length !== 1) {
+        throw new Error('Platform currently support single demand LLM');
+      }
+
+      return allDemands.reduce(
+        (memo, demandKey) => {
+          memo.llm_fulfillments[demandKey] = {
             identifier: 'llm_proxy',
             api_base: '{platform_url}/api/v1/llm/',
             api_key: platformToken,
             api_model: 'dummy',
-          },
+          };
+
+          return memo;
         },
-      };
+        { llm_fulfillments: {} },
+      );
     },
   };
 };
