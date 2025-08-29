a6e5f0801af1cdb8716b0277bf016ad61a57e034
Chintan Soni <ichintansoni@gmail.com>
2025-08-29 20:13:16 +0530

    fix(web): update image import in Platform component
    Signed-off-by: Chintan Soni <ichintansoni@gmail.com>

--- apps/beeai-web/beeai-env.d.ts
+++ apps/beeai-web/beeai-env.d.ts
@@ -3,6 +3,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
 declare module '*.svg' {
   import { FC, SVGProps } from 'react';
   const content: FC<SVGProps<SVGElement>>;
