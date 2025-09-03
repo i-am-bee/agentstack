964fc78a6b952d0e152c599d56985834eba7bcb0
habeck <habeck@us.ibm.com>
2025-09-03 10:38:23 -0400

    chore: update initials extractor in UserAvatar
    Signed-off-by: habeck <habeck@us.ibm.com>

--- apps/beeai-ui/src/components/Avatar/UserAvatar.tsx
+++ apps/beeai-ui/src/components/Avatar/UserAvatar.tsx
@@ -5,13 +5,20 @@
 
 import { useSession } from 'next-auth/react';
 
+const getUserInitials = (name: string) => {
+  if (!name) return '';
+
+  // Names can have unicode characters in them, use unicode aware regex
+  const matches = [...name.matchAll(/(\p{L}{1})\p{L}+/gu)];
+  const initials = (matches.shift()?.[1] ?? '') + (matches.pop()?.[1] ?? '');
+  return initials.toUpperCase();
+};
+
 export default function UserAvatar() {
   const { data: session } = useSession();
   let userInitials = '';
   if (session?.user?.name) {
-    const namesArray = session.user.name.split(' ');
-    const lastIndex = namesArray.length - 1;
-    userInitials = namesArray[0][0] + ' ' + namesArray[lastIndex][0];
+    userInitials = getUserInitials(session.user.name);
   }
   return (
     <div>
