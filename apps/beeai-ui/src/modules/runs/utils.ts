9a301dafee18b966ea82e488787c524840ebdf4b
Petr Bulánek <bulanek.petr@gmail.com>
2025-09-05 10:35:32 +0200

    fixup! feat(ui): show code block only for json-like string in trajectory description
    Signed-off-by: Petr Bulánek <bulanek.petr@gmail.com>

--- apps/beeai-ui/src/modules/runs/utils.ts
+++ apps/beeai-ui/src/modules/runs/utils.ts
@@ -26,7 +26,7 @@ export function runDuration(ms: number) {
   return duration;
 }
 
-const parseJsonLikeString = (string: string): unknown | string => {
+const parseJsonLikeString = (string: string): unknown => {
   try {
     const json = JSON5.parse(string);
 
@@ -46,26 +46,26 @@ export const maybeParseJson = (content: string | null | undefined): MaybeParsedJ
     return null;
   }
 
-  try {
-    const maybeJson = parseJsonLikeString(content);
+  const maybeJson = parseJsonLikeString(content);
 
-    if (typeof maybeJson === 'string') {
-      return {
-        type: 'string',
-        value: maybeJson,
-      };
-    }
+  if (typeof maybeJson === 'string') {
+    return {
+      type: 'string',
+      value: maybeJson,
+    };
+  }
+
+  try {
+    const json = JSON.stringify(maybeJson, null, 2);
 
     return {
       type: 'json',
-      value: JSON.stringify(maybeJson, null, 2),
+      value: json,
     };
   } catch {
-    // noop
+    return {
+      type: 'string',
+      value: content,
+    };
   }
-
-  return {
-    type: 'string',
-    value: content,
-  };
 };
