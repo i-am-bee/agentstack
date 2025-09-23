ec7e156e8148af385923f9442c9bd504632b50ed
Petr Bulánek <bulanek.petr@gmail.com>
2025-09-23 15:02:46 +0200

    chore(ui): add stylelint-config as separate workspace package
    Signed-off-by: Petr Bulánek <bulanek.petr@gmail.com>

---
+++ apps/beeai-web/stylelint.config.js
@@ -0,0 +1,11 @@
+/**
+ * Copyright 2025 © BeeAI a Series of LF Projects, LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/** @type {import('stylelint').Config} */
+const config = {
+  extends: ['@i-am-bee/stylelint-config'],
+};
+
+export default config;
