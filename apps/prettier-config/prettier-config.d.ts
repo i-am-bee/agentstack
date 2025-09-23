24feed9a46243e37b467c95d54df7f25c12a7982
Petr Bulánek <bulanek.petr@gmail.com>
2025-09-23 17:31:48 +0200

    fixup! fixup! fixup! fixup! chore(ui): add stylelint-config as separate workspace package
    Signed-off-by: Petr Bulánek <bulanek.petr@gmail.com>

---
+++ apps/prettier-config/prettier-config.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Copyright 2025 © BeeAI a Series of LF Projects, LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+declare module '@i-am-bee/prettier-config' {
+  import type { Config } from 'prettier';
+
+  const config: Config;
+
+  export default config;
+}
