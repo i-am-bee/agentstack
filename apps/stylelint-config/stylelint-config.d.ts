7ff51c5bd2d249a97d22ee2c83fcb26aa284578a
Petr Bulánek <bulanek.petr@gmail.com>
2025-09-24 11:30:43 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! chore(ui): add stylelint-config as separate workspace package
    Signed-off-by: Petr Bulánek <bulanek.petr@gmail.com>

---
+++ apps/stylelint-config/stylelint-config.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Copyright 2025 © BeeAI a Series of LF Projects, LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+declare module '@i-am-bee/stylelint-config' {
+  import type { Config } from 'stylelint';
+
+  const config: Config;
+
+  export default config;
+}
