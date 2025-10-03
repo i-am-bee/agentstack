01af3d5aed754641b8e6bd8e42e92043633636aa
Tomas Pilar <thomas7pilar@gmail.com>
2025-10-03 14:17:40 +0200

    fixup!
    Signed-off-by: Tomas Pilar <thomas7pilar@gmail.com>

--- apps/beeai-server/src/beeai_server/api/auth/utils.py
+++ apps/beeai-server/src/beeai_server/api/auth/utils.py
@@ -5,4 +5,4 @@ from starlette.datastructures import URL
 
 
 def create_resource_uri(url: URL) -> str:
-    return f"{url.scheme}://{url.netloc}/{url.path}".rstrip("/")
+    return f"{url.scheme}://{url.netloc}{url.path}".rstrip("/")
