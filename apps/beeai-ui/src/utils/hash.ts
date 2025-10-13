1811fd3a6b89e45f6f6965a9ae380713cc11fe29
Petr Kadlec <petr@puradesign.cz>
2025-10-13 16:35:36 +0200

    fix(ui): add edge runtime compatible hash function for auth cache
    Signed-off-by: Petr Kadlec <petr@puradesign.cz>

---
+++ apps/beeai-ui/src/utils/hash.ts
@@ -0,0 +1,12 @@
+/**
+ * Copyright 2025 © BeeAI a Series of LF Projects, LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export async function sha256Hash(input: string): Promise<string> {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(input);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  return hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
+}
