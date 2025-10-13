38f6a49ec9da8b6dc716b1ade9aa3992ee5d8540
Petr Kadlec <petr@puradesign.cz>
2025-10-13 16:40:27 +0200

    fixup! review comment
    Signed-off-by: Petr Kadlec <petr@puradesign.cz>

--- apps/beeai-ui/src/utils/hash.ts
+++ apps/beeai-ui/src/utils/hash.ts
@@ -7,6 +7,10 @@ export async function sha256Hash(input: string): Promise<string> {
   const encoder = new TextEncoder();
   const data = encoder.encode(input);
   const hashBuffer = await crypto.subtle.digest('SHA-256', data);
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  return hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
+  const hashBytes = new Uint8Array(hashBuffer);
+  let hexString = '';
+  for (const byte of hashBytes) {
+    hexString += byte.toString(16).padStart(2, '0');
+  }
+  return hexString;
 }
