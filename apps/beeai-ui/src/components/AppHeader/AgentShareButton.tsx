6abc1d0ec073a4349d102e9a8d341988cafba0b9
Petr Kadlec <petr@puradesign.cz>
2025-10-14 14:26:25 +0200

    fixup! substitute success tooltip with notification
    Signed-off-by: Petr Kadlec <petr@puradesign.cz>

--- apps/beeai-ui/src/components/AppHeader/AgentShareButton.tsx
+++ apps/beeai-ui/src/components/AppHeader/AgentShareButton.tsx
@@ -5,11 +5,9 @@
 
 import { Share } from '@carbon/icons-react';
 import { Button } from '@carbon/react';
-import type { RefObject } from 'react';
-import { useRef, useState } from 'react';
-import { useCopyToClipboard, useOnClickOutside } from 'usehooks-ts';
+import { useCopyToClipboard } from 'usehooks-ts';
 
-import { Tooltip } from '#components/Tooltip/Tooltip.tsx';
+import { useToast } from '#contexts/Toast/index.ts';
 import type { Agent } from '#modules/agents/api/types.ts';
 import { routes } from '#utils/router.ts';
 
@@ -19,21 +17,16 @@ interface Props {
 
 export function AgentShareButton({ agent }: Props) {
   const [, copy] = useCopyToClipboard();
-  const buttonRef = useRef<HTMLButtonElement>(null);
-  const [showCopied, setShowCopied] = useState(false);
-
-  useOnClickOutside(buttonRef as RefObject<HTMLButtonElement>, () => setShowCopied(false));
+  const { addToast } = useToast();
 
   const handleShare = () => {
     copy(`${window.location.origin}${routes.agentRun({ providerId: agent.provider.id })}`);
-    setShowCopied(true);
+    addToast({ kind: 'info', title: 'Link has been copied to clipboard!' });
   };
 
   return (
-    <Tooltip content="Link has been copied to clipboard!" isOpen={showCopied} placement="bottom" size="sm" asChild>
-      <Button kind="tertiary" size="sm" renderIcon={Share} onClick={handleShare} ref={buttonRef}>
-        Share agent
-      </Button>
-    </Tooltip>
+    <Button kind="tertiary" size="sm" renderIcon={Share} onClick={handleShare}>
+      Share agent
+    </Button>
   );
 }
