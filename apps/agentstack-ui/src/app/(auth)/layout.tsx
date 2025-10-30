i-am-bee/beeai-platform

refactor(ui): sign in page and its components
Signed-off-by: Petr Bulánek <bulanek.petr@gmail.com>

apps/beeai-ui/src/app/(auth)/layout.tsx
@@ -5,12 +5,12 @@
 
 import '#styles/style.scss';
 
-import { SigninLayout } from '#components/layouts/SigninLayout.tsx';
+import { SignInLayout } from '#components/layouts/SignInLayout.tsx';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <SigninLayout>{children}</SigninLayout>;
+  return <SignInLayout>{children}</SignInLayout>;
 }

i-am-bee/agentstack

apps/agentstack-ui/src/app/(auth)/layout.tsx
@@ -5,12 +5,12 @@
 
 import '#styles/style.scss';
 
-import { SigninLayout } from '#components/layouts/SigninLayout.tsx';
+import { SignInLayout } from '#components/layouts/TMP_SignInLayout.tsx';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <SigninLayout>{children}</SigninLayout>;
+  return <SignInLayout>{children}</SignInLayout>;
 }