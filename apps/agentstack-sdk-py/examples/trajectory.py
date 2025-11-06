eb3a97798949606e730c896b01f4f72cee89b4e9
Petr Kadlec <petr@puradesign.cz>
2025-11-06 15:14:26 +0100

    fixup! py format fix
    Signed-off-by: Petr Kadlec <petr@puradesign.cz>

--- apps/agentstack-sdk-py/examples/trajectory.py
+++ apps/agentstack-sdk-py/examples/trajectory.py
@@ -74,6 +74,7 @@ async def example_agent(
     # Store the message in the context store
     await context.store(message)
 
+
 def run():
     server.run(
         host=os.getenv("HOST", "127.0.0.1"), port=int(os.getenv("PORT", 8000)), context_store=PlatformContextStore()
