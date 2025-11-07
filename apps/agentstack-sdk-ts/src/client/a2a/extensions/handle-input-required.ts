60f6359ea9520ef7feb887f5aa65b8d0098d3e5b
Tomas Weiss <tomas.weiss2@gmail.com>
2025-11-07 09:56:54 +0100

    fix: missing headers
    Signed-off-by: Tomas Weiss <tomas.weiss2@gmail.com>

--- apps/agentstack-sdk-ts/src/client/a2a/extensions/handle-input-required.ts
+++ apps/agentstack-sdk-ts/src/client/a2a/extensions/handle-input-required.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2025 © BeeAI a Series of LF Projects, LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import type { FormResponseValue } from './common/form';
 import { requestFormExtension } from './ui/request-form';
 
