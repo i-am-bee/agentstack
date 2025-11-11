7e05dc1b405dd72b45e4a93d519a60bfc01af4fe
Petr Bulánek <bulanek.petr@gmail.com>
2025-11-11 13:33:19 +0100

    fixup! fix(ui): array length checks
    Signed-off-by: Petr Bulánek <bulanek.petr@gmail.com>

--- apps/agentstack-ui/src/modules/agents/components/detail/AgentSecrets.tsx
+++ apps/agentstack-ui/src/modules/agents/components/detail/AgentSecrets.tsx
@@ -12,11 +12,11 @@ import classes from './AgentSecrets.module.scss';
 export function AgentSecrets() {
   const { demandedSecrets } = useAgentSecrets();
 
-  const hasSecretss = demandedSecrets.length > 0;
+  const hasSecrets = demandedSecrets.length > 0;
 
   return (
     <div className={classes.root}>
-      {hasSecretss ? (
+      {hasSecrets ? (
         <ul className={classes.list}>
           {demandedSecrets.map((secret) => (
             <li key={secret.key}>
