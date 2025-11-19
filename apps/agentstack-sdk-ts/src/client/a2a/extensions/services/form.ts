df38d8b4db2714b521d10ed81a85fb8990adcb97
Petr Bulánek <bulanek.petr@gmail.com>
2025-11-19 14:53:50 +0100

    fixup! fix: code review
    Signed-off-by: Petr Bulánek <bulanek.petr@gmail.com>

--- apps/agentstack-sdk-ts/src/client/a2a/extensions/services/form.ts
+++ apps/agentstack-sdk-ts/src/client/a2a/extensions/services/form.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { z } from 'zod';
+import z from 'zod';
 
 import { formRenderSchema, formResponseSchema } from '../common/form';
 import type { A2AServiceExtension } from '../types';
