691b6e11b8cbe0dab29404d1d0c51ab7b0c6494c
Aleš Kalfas <kalfas.ales@gmail.com>
2026-02-04 09:44:11 +0100

    fixup!
    Signed-off-by: Aleš Kalfas <kalfas.ales@gmail.com>

--- apps/agentstack-server/tests/e2e/examples/agent-integration/forms/test_initial_form_rendering.py
+++ apps/agentstack-server/tests/e2e/examples/agent-integration/forms/test_initial_form_rendering.py
@@ -12,7 +12,7 @@ from tests.e2e.examples.conftest import run_example
 pytestmark = pytest.mark.e2e
 
 
-@pytest.mark.usefixtures("clean_up")
+@pytest.mark.usefixtures("clean_up", "setup_platform_client")
 async def test_initial_form_rendering_example(subtests, get_final_task_from_stream, a2a_client_factory):
     example_path = "agent-integration/forms/initial-form-rendering"
 
